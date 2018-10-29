38c5f465373cccb2f324ab69b766f9101686b488
Klint Mane <klintmane@gmail.com>
2018-10-29 12:54:25 +0100

    New Build

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "../build"),
+    libraryTarget: "umd",
     publicPath: "/",
     filename: "[name].js",
     chunkFilename: "[name].js"
