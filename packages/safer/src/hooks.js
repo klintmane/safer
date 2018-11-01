b6ca1c4fa3f3185df021419cf169f1d843d6fccf
Klint Mane <klintmane@gmail.com>
2018-11-01 16:42:52 +0100

    New build

--- packages/safer/src/hooks.js
+++ packages/safer/src/hooks.js
@@ -2,7 +2,9 @@ import { useState, useEffect, useContext } from "react";
 import { getContext } from "./context";
 
 export const useStores = (...keys) => {
-  const contexts = keys.map(key => useContext(getContext(key)));
+  const contexts = keys.map(key =>
+    useContext(getContext(key) || getContext("__no_context"))
+  );
   const [stores, setStores] = useState(contexts);
 
   // Run only once
