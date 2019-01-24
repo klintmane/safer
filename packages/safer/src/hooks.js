d6ccaf477bda0732563502251139b1c47904808e
Klint Mane <klintmane@gmail.com>
2019-01-24 23:42:32 +0100

    Small hook fix

--- packages/safer/src/hooks.js
+++ packages/safer/src/hooks.js
@@ -1,14 +1,10 @@
-import { useState, useEffect, useContext } from "react";
+import { useContext } from "react";
 import { getContext } from "./context";
 
 export const useStores = (...keys) => {
-  const contexts = keys.map(key =>
+  const stores = keys.map(key =>
     useContext(getContext(key) || getContext("__no_context"))
   );
-  const [stores, setStores] = useState(contexts);
-
-  // Run only once
-  useEffect(() => {}, []);
 
   return stores;
 };
